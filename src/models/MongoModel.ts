17f3eef3f96010214ca3c579087165fb8366feb6
Juliane <julianesilvac75@gmail.com>
2022-09-12 22:49:51 +0100

    exercise 6: destroy method on the model

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -20,6 +20,11 @@ abstract class MongoModel<T> implements IModel<T> {
   public async read(): Promise<T[]> {
     return this._model.find({});
   }
+
+  public async destroy(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findByIdAndDelete({ _id });
+  }
 }
 
 export default MongoModel;
