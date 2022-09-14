c1578848e51377ecf0a1060db260a2ca36d60a18
Juliane <julianesilvac75@gmail.com>
2022-09-14 21:48:17 +0100

    app

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -1,12 +1,10 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-// eslint-disable-next-line max-len
-const connect = 'mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb';
-
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
-    || connect,
+    // eslint-disable-next-line max-len
+    || 'mongodb://127.0.0.1:27017/glassesStore?readPreference=primary&directConnection=true&ssl=false',
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
